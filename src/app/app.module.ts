c00d0bef7f6aa9471a3cbdeb23aaa02f3ed34029
rkelder <roderik.kelder@gmail.com>
2023-11-06 20:52:05 +0100

    Removed flashcardlistoverview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FlashcardsComponent } from './flashcards/flashcards.component';
-import { FlashcardListOverviewComponent } from './flashcard-list-overview/flashcard-list-overview.component';
 import { FlashcardComponent } from './flashcards/flashcard/flashcard.component';
-import { FlashcardListsComponent } from './flashcard-list-overview/flashcard-lists/flashcard-lists.component';
-import { FlashcardListDetailComponent } from './flashcard-list-overview/flashcard-list-detail/flashcard-list-detail.component';
-import { FlashcardListItemComponent } from './flashcard-list-overview/flashcard-lists/flashcard-list-item/flashcard-list-item.component';
 import { FlashcardListSelectComponent } from './flashcards/flashcard-list-select/flashcard-list-select.component';
 
 @NgModule({
@@ -16,11 +12,7 @@ import { FlashcardListSelectComponent } from './flashcards/flashcard-list-select
     AppComponent,
     HeaderComponent,
     FlashcardsComponent,
-    FlashcardListOverviewComponent,
     FlashcardComponent,
-    FlashcardListsComponent,
-    FlashcardListDetailComponent,
-    FlashcardListItemComponent,
     FlashcardListSelectComponent,
   ],
   imports: [BrowserModule],
